3ea1d64f941e8524a1ac66eae6c47b985de5a42f
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-06-09 15:01:31 +0300

    commit

--- res/script.js
+++ res/script.js
@@ -4,14 +4,24 @@ let rowContainer = document.querySelector(".row-container");
 console.log(rowContainer);
 let num = 100;
 
+const insertHTML = (state, i) => {
+  let html = `
+        <tr class="row">
+            <td>${i}</td>
+            <td>${state}</td>
+        </tr>
+    `;
+  rowContainer.insertAdjacentHTML("beforeend", html);
+};
+
 for (let i = 1; i <= num; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("fizzBuzz");
+    insertHTML("fizzBUZZ", i);
   } else if (i % 5 === 0) {
-    console.log("Fizz");
+    insertHTML("Fizz", i);
   } else if (i % 3 === 0) {
-    console.log("Buzz");
+    insertHTML("Buzz", i);
   } else {
-    console.log(i);
+    insertHTML(i, i);
   }
 }

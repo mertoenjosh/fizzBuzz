acf71fd3a6b7973fe85f5e941ddb7f3d234ded26
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-06-09 12:48:03 +0300

    commit

---
+++ res/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let num = 100;
+
+for (let i = 1; i <= num; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Fizz");
+  } else if (i % 3 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}

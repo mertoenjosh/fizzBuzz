a8be1c3d61d62fff55860d7b70838e3bf2545b37
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-06-10 23:36:28 +0300

    commit

--- res/script.js
+++ res/script.js
@@ -1,9 +1,18 @@
 "use strict";
 
+let num = parseInt(prompt("Enter number: "));
+
+if (!Number.isNaN(num)) {
+  console.log(num);
+} else {
+  alert(`${num} is not a number\nWe have initialized 100 for you`);
+  num = 100;
+}
+
 let rowContainer = document.querySelector(".row-container");
 console.log(rowContainer);
 
-let num = 100;
+// let num = 100;
 
 const insertHTML = (state, i, bgClass = "") => {
   let html = `
